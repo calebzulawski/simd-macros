ff3bf721c2a23278010166498105e09522739b2d
Caleb Zulawski <caleb.zulawski@gmail.com>
2024-03-20 00:24:06 -0400

    Add else-if to test

--- tests/vectorize.rs
+++ tests/vectorize.rs
@@ -5,6 +5,8 @@ fn foo(x: core::simd::f32x4, y: core::simd::u32x4) -> core::simd::f32x4 {
     simd_macros::vectorize!(4, {
         if y == 1 {
             x
+        } else if y == 2{
+            x + 1.0
         } else {
             x + y as f32
         }
